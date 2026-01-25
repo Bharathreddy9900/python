30c52638158d526cdbee4b9305d950deb20beb76
Bharath R K <bharatreddy9900@gmail.com>
2026-01-25 22:02:16 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+#string can be represented in single ,double,triple quotes
+name='bharat'
+name2="bharat"
+name="""bharat"""
+
+'''string slicing
+syntax for string slicing is sl=name[ind_start:ind_end]
+    ex:sl[3:5]'''
+a="bharath"
+b=a[1:5]
+print(b)
