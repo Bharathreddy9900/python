8ded449190a08a699e79fef44f2fa299a6eb8999
Bharath R K <bharatreddy9900@gmail.com>
2026-01-29 23:28:04 +0530

    Add files via upload

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 #Variables are like container they store values (i.e it can be any data types like strings ,numbers).
-a=2
+a="2"
 b="Bharath"
 c="i am good."
 print(c)
@@ -7,10 +7,28 @@ print(c)
 1.variables can start with letters and underscore.
 2.variables can't start with digits 
 3.no space is allowed
-4.variables can contain numbers,alphabets,underscores(NO OTHER CHARACTERS)  """
+4.variables can contain numbers,alphabets,underscores(NO OTHER CHARACTERS) 
+5.variables are case sensitive (i.e age,Age,AGE) all are different variables"""
 #examples are
 variable=1
 _variable=3
 #@variable=3 is not allowed 
 var123=34
+
+#multiple values can be assigned 
+z=y=x="orange"
+print(z)
+print(y)
+print(x)
+
+#When there is tuple or lists you can extract the values this is called [unpacking]
+cars=["BMW","AUDI","BENZ"]
+Q,R,S=cars
+print(Q)
+print(R)
+print(S)
+
+
+
+print(a+b+c)
 
