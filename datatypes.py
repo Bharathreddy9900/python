4e009fe839e22e53576a41233820e1d0f3e4d5ac
Bharath R K <bharatreddy9900@gmail.com>
2026-01-30 22:42:54 +0530

    Add files via upload

--- datatypes.py
+++ datatypes.py
@@ -4,3 +4,18 @@ a=1  # a is integer
 b="bharath"#b stores string
 c=0.060#c stores float value
 d=True#stores boolean value
+print(type(c))
+
+"""data types:
+text  = str
+numeric type =int,float,complex
+sequence type = list,tuple,range
+mapping type=dict
+set types=set,frozenset
+boolean type=bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType"""
+
+x=int(c)
+print(x)
+print(type(x))
---
+++ random.py
@@ -0,0 +1,6 @@
+'''In python there is no RANDOM FUNCTION() to make random number but python has built in module'''
+'''import random
+print(random.randrange(1,1056))'''
+import random
+
+print(random.randrange(1, 10))
