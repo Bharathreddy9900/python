b0ad2b046f6ff75291b0b261a241b5357446f3d7
Bharath R K <bharatreddy9900@gmail.com>
2026-01-31 23:57:23 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,11 @@
+'''python casting is a method to convert the python variable datatype into certain different datatype '''
+a=float(34)
+print(a)
+b=int(34.8923)
+print(b)
+
+
+'''implicit type conversion'''
+#here  python itself converts automatically converts one data type to other during an operation to ensur correct and safe evaluation,without requiring any action from user
+a=23
+print(type(a))
--- strings.py
+++ strings.py
@@ -4,8 +4,44 @@ name2="bharat"
 name="""bharat"""
 
 '''string slicing
-syntax for string slicing is sl=name[ind_start:ind_end]
+syntax for string slicing is sl=name[ind_start:ind_end],even negative indexing can be used
     ex:sl[3:5]'''
-a="bharath"
+a="   bharath ,reddy  "
+print(a[:5])#from starting
+print(a[2:])
 b=a[1:5]
 print(b)
+
+'''
+Python has some built-in functions which could be used '''
+#upper()
+print(a.upper())
+#lower()
+print(a.lower())
+#strip()
+#here it removes the white spaces at the atarting and end of the string
+print(a)#here it returns "bharath"
+#replace()
+print(a.replace("h","e"))
+#split()
+print(a.split(","))
+
+"""concatinate strings"""
+
+c="bharath"
+d="reddy"
+e=c+d#combining strings
+print(e)
+f=c+" "+d#adding space
+print(f)
+
+
+"""FORMAT STRINGS"""
+#earlier it was difficult to add strings and numbers ,to specify f-strings we can use "f"infront of string literaland add curly brackets{}
+#place holder can contain variables,functions,operationsand modifiers
+age=21
+txt=f"I am bharath,and i am {age}years old"
+print(txt)
+xtx=f"price of carrot is{56*80}"
+print(xtx)
+
